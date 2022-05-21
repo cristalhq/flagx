625f487919a002f102805b84b63f6650dbf3bd7a
Oleg Kovalov <oleg@hey.com>
2022-05-21 12:44:09 +0200

    Update example

--- example_test.go
+++ example_test.go
@@ -1,24 +1,24 @@
 package flagx_test
 
 import (
+	"fmt"
 	"os"
-	"testing"
 	"time"
 
 	"github.com/cristalhq/flagx"
 )
 
-func ExampleFlagSet(t *testing.T) {
+func ExampleFlagSet() {
 	var d time.Duration
 	fset := flagx.NewFlagSet("testing", os.Stderr)
 	fset.Duration(&d, "timeout", "t", 10*time.Second, "just a timeout")
 
 	err := fset.Parse([]string{"-t", "20s"})
 	if err != nil {
-		t.Fatal(err)
+		panic(err)
 	}
 
 	if d != 20*time.Second {
-		t.Fatalf("got %v want %v", d, 20*time.Second)
+		panic(fmt.Sprintf("got %v want %v", d, 20*time.Second))
 	}
 }
